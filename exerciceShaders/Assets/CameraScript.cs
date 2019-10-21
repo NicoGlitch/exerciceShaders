c80e6ede5d0f6c1b34db7cc9eaa2bb0cdbd620d8
asd <asd@hotmail.com>
2019-10-21 17:06:49 -0400

    exerciceTerminé

---
+++ exerciceShaders/Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraScript : MonoBehaviour {
+    private Material material;
+	// Use this for initialization
+	void Awake () {
+        material = new Material(Shader.Find("Hidden/NewImageEffectShaderTEST"));
+	}
+	
+	// Update is called once per frame
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, material);
+    }
+}
